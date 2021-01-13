480b53516fd6f9272e9516b0ccbdce264a6408a7
Vincent Farah <vincent.farah@madetech.com>
2021-01-13 11:22:06 +0000

    Create quanitity input with test

--- corn-calculator/src/App.js
+++ corn-calculator/src/App.js
@@ -4,19 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">        
+        <h1>
+          Corn calculator
+        </h1>
+      <label forhtml="quantity">Quantity:</label>
+      <input type="number" name="quantity"></input>      
       </header>
     </div>
   );
--- corn-calculator/src/App.test.js
+++ corn-calculator/src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('given APP', () => {
+  test('should contain a header with corn calculator', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Corn calculator/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('renders create a quantity input', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Quantity/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  
 });
