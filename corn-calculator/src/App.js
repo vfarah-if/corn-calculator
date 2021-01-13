08da5c8392e2e2b235df3a813fdefc618f9a63ae
Vincent Farah <vincent.farah@madetech.com>
2021-01-13 12:31:21 +0000

    Created test and basic calculator

--- corn-calculator/src/App.js
+++ corn-calculator/src/App.js
@@ -1,15 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [quantity, setQuantity] = useState(1);
+  const [costPerBag, setCostPerBag] = useState(0.25);
+  const onQuantityChange = (e) => {
+    console.log("Test", e.target.value);
+    setQuantity(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">        
-        <h1>
-          Corn calculator
-        </h1>
-      <label forhtml="quantity">Quantity:</label>
-      <input type="number" name="quantity"></input>      
+      <header className="App-header">
+        <h1>Corn calculator</h1>
+        <label forhtml="quantity">Quantity:</label>
+        <input
+          type="number"
+          name="quantity"
+          value={quantity}
+          onChange={onQuantityChange}
+        ></input>
+        <label>£ {quantity * costPerBag}</label>
       </header>
     </div>
   );
--- corn-calculator/src/App.test.js
+++ corn-calculator/src/App.test.js
@@ -1,17 +1,41 @@
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
 
-describe('given APP', () => {
+describe('APP', () => {
   test('should contain a header with corn calculator', () => {
     render(<App />);
     const linkElement = screen.getByText(/Corn calculator/i);
     expect(linkElement).toBeInTheDocument();
   });
 
-  test('renders create a quantity input', () => {
+  test('should contain a quantity label', () => {
     render(<App />);
     const linkElement = screen.getByText(/Quantity/i);
     expect(linkElement).toBeInTheDocument();
   });
-  
+
+  test('should contain a quantity with initial default value of 1', () => {
+    const component = render(<App />);
+    
+    const inputElement = component.container.querySelector('input');
+
+    expect(inputElement).toBeTruthy();
+    expect(inputElement.value).toBe("1");
+  });
+
+  test('should calculate the cost of 1 bag of corn at 25 pence', () => {
+    render(<App />);
+    
+    const linkElement = screen.getByText(/£ 0.25/i);
+    
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('should get default app in the expected html format', () => {
+    const component = render(<App />);
+          
+    expect(component.container).toMatchSnapshot();
+  });
+
 });
