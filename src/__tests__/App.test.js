77a05dae9b5708e7e6da8b4782e065112f57a520
Vincent Farah <vincent.farah@madetech.com>
2021-01-14 09:16:44 +0000

    Refactored test structure

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 
-import App from './App';
+import App from '../App';
 
 describe('App', () => {
   test('should contain a header with river trip adviser', () => {
