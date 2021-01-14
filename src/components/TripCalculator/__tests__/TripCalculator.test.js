397352750cbe217cb9b79d0145bd254b33865353
Vincent Farah <vincent.farah@madetech.com>
2021-01-14 13:27:10 +0000

    0 goose, 2 corn test

--- src/components/TripCalculator/__tests__/TripCalculator.test.js
+++ src/components/TripCalculator/__tests__/TripCalculator.test.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { render } from "@testing-library/react";
 
-import { DefaultZeros, OneGooseAndZeroCorn, OneCornAndZeroGoose } from "../TripCalculator.stories";
+import {
+  DefaultZeros,
+  OneGooseAndZeroCorn,
+  OneCornAndZeroGoose,
+  TwoCornAndZeroGoose,
+} from "../TripCalculator.stories";
 
 describe("Trip Calculator", () => {
   test("should default with 0 goose, 0 corn : cost £0.25 : no cargo to take", () => {
@@ -11,14 +16,26 @@ describe("Trip Calculator", () => {
   });
 
   test("should get 1 goose, 0 corn : cost £0.25 : take in this order : goose", () => {
-    const { container } = render(<OneGooseAndZeroCorn {...OneGooseAndZeroCorn.args} />);
+    const { container } = render(
+      <OneGooseAndZeroCorn {...OneGooseAndZeroCorn.args} />
+    );
 
     expect(container).toMatchSnapshot("one goose only");
   });
 
   test("should get 0 goose, 1 corn : cost £0.25 : take in this order : corn", () => {
-    const { container } = render(<OneCornAndZeroGoose {...OneCornAndZeroGoose.args} />);
+    const { container } = render(
+      <OneCornAndZeroGoose {...OneCornAndZeroGoose.args} />
+    );
 
     expect(container).toMatchSnapshot("one corn only");
+  });
+
+  test("should get 0 goose, 2 corn : cost £0.50 : take in this order : corns", () => {
+    const { container } = render(
+      <TwoCornAndZeroGoose {...TwoCornAndZeroGoose.args} />
+    );
+
+    expect(container).toMatchSnapshot("two corn only");
   });
 });
