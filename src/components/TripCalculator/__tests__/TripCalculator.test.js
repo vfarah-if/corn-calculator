9fd6fbdd14f554c819dbdfd44699a653f65a65fc
Vincent Farah <vincent.farah@madetech.com>
2021-01-14 13:22:07 +0000

    1 goose, 0 corn test

--- src/components/TripCalculator/__tests__/TripCalculator.test.js
+++ src/components/TripCalculator/__tests__/TripCalculator.test.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { render } from "@testing-library/react";
 
-import { DefaultZeros } from "../TripCalculator.stories";
+import { DefaultZeros, OneGooseAndZeroCorn } from "../TripCalculator.stories";
 
 describe("Trip Calculator", () => {
   test("should default with 0 goose, 0 corn : cost £0.25 : no cargo to take", () => {
     const { container } = render(<DefaultZeros {...DefaultZeros.args} />);
 
     expect(container).toMatchSnapshot("default zeros");
+  });
+
+  test("should get 1 goose, 0 corn : cost £0.25 : take in this order : goose", () => {
+    const { container } = render(<OneGooseAndZeroCorn {...OneGooseAndZeroCorn.args} />);
+
+    expect(container).toMatchSnapshot("one goose only");
   });
 });
